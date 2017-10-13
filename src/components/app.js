3142c73a75e84f26b62dbb4c2dc4fc8f006b3931
Cooper <cooper.larson1@gmail.com>
2017-10-12 21:48:03 -0500

    Initial commit

---
+++ src/components/app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Component } from 'react';
+
+import WeatherList from '../containers/weather_list';
+import SearchBar from '../containers/search_bar';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <WeatherList />
+      </div>
+    );
+  }
+}
